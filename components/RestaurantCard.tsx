bbb288d3b7297c2641c523b4312833546e729346
khalifa47 <khalifafumo5@gmail.com>
2022-12-25 13:45:46 +0300

    featured rows done

---
+++ components/RestaurantCard.tsx
@@ -0,0 +1,55 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { StarIcon } from "react-native-heroicons/solid";
+import { MapPinIcon } from "react-native-heroicons/outline";
+import React from "react";
+
+const RestaurantCard = ({
+  id,
+  imgUrl,
+  title,
+  rating,
+  genre,
+  address,
+  short_desc,
+  dishes,
+  long,
+  lat,
+}: {
+  id: number;
+  imgUrl: string;
+  title: string;
+  rating: number;
+  genre: string;
+  address: string;
+  short_desc: string;
+  dishes: Array<string>;
+  long: number;
+  lat: number;
+}) => {
+  return (
+    <TouchableOpacity className="bg-white mr-3 shadow rounded">
+      <Image
+        source={{
+          uri: imgUrl,
+        }}
+        className="h-36 w-64 rounded-sm"
+      />
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <StarIcon color="green" size={22} opacity={0.5} />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-green-500">{rating}</Text> • {genre}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center space-x-1">
+          <MapPinIcon color="gray" size={22} opacity={0.4} />
+          <Text className="text-xs text-gray-500">Nearby • {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default RestaurantCard;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,6 +3,7 @@ import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import Header from "../components/Header";
 import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -14,15 +15,31 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView className="bg-white pt-5">
+    <SafeAreaView className="bg-gray-100 pt-5">
       <Header />
 
       {/* Body */}
       <ScrollView>
-        {/* Categories */}
         <Categories />
 
         {/* Featured Rows */}
+        <FeaturedRow
+          id="1"
+          title="Featured"
+          desc="Paid placements from our partners"
+        />
+
+        <FeaturedRow
+          id="2"
+          title="Tasty discounts"
+          desc="Enjoy up to 50% discount"
+        />
+
+        <FeaturedRow
+          id="3"
+          title="Offers near you"
+          desc="Support your local restaurant"
+        />
       </ScrollView>
     </SafeAreaView>
   );
